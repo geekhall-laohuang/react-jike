9f3eadb09a9dad65f2b1653f98874132d5bb9504
Kevin-Huang <2919936385@qq.com>
2024-10-31 14:38:29 +0800

    路由懒加载优化

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,15 @@ import Layout from "@/pages/Layout";
 
 import { createBrowserRouter } from "react-router-dom";
 import { AuthRoute } from "@/components/AuthRoute";
-import Article from "@/pages/Article";
-import Home from "@/pages/Home";
-import Publish from "@/pages/Publish";
+// import Article from "@/pages/Article";
+// import Home from "@/pages/Home";
+// import Publish from "@/pages/Publish";
+
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("@/pages/Home"));
+const Article = lazy(() => import("@/pages/Article"));
+const Publish = lazy(() => import("@/pages/Publish"));
 
 const router = createBrowserRouter([
   {
@@ -16,9 +22,30 @@ const router = createBrowserRouter([
       </AuthRoute>
     ),
     children: [
-      { index: true, element: <Home /> },
-      { path: "article", element: <Article /> },
-      { path: "publish", element: <Publish /> },
+      {
+        index: true,
+        element: (
+          <Suspense fallback="加载中">
+            <Home />
+          </Suspense>
+        ),
+      },
+      {
+        path: "article",
+        element: (
+          <Suspense fallback="加载中">
+            <Article />
+          </Suspense>
+        ),
+      },
+      {
+        path: "publish",
+        element: (
+          <Suspense fallback="加载中">
+            <Publish />
+          </Suspense>
+        ),
+      },
     ],
   },
   {
